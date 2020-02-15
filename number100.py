6ed47928ba561b339c6a166a5ed15c4ee99a5978
weiJinzhou <com-1919@163.com>
2020-02-15 10:25:08 +0800

    更改了输出内容，“It\’s smaller..”更改为“The number is less...”，“It\'s equal ..”更改为“The number is equal...”，“It\‘s bigger ..”更改为“The number is greater than...”

--- number100.py
+++ number100.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 number = int(input("Please enter a number: "))
 if number < 100:
-    print("It\'s smaller than 100.")
+    print("The number is less than 100.")
 elif number == 100:
-    print("It\'s equal to 100.")
+    print("The number is equal to 100.")
 else:
-    print("It\'s bigger than 100.")
+    print("The number is greater than 100.")
