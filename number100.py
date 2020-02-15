544e453bdb235496a46d78dcff2bab604d61d5ec
weiJinzhou <com-1919@163.com>
2020-02-15 08:56:42 +0800

    first commit

---
+++ number100.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+number = int(input("Please enter a number: "))
+if number < 100:
+    print("It\'s smaller than 100.")
+elif number == 100:
+    print("It\'s equal to 100.")
+else:
+    print("It\'s bigger than 100.")
