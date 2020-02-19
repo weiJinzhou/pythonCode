5bc1c38a83f9335b46c642da0c10df593d90e691
weiJinzhou <com-1919@163.com>
2020-02-19 15:28:05 +0800

    first commit

---
+++ download.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import requests
+
+def download(url):
+    """
+    从指定的 URL 中下载文件并存储到当前目录
+    url：要下载页面内容的网址
+    """
+    # 检查 URL 是否存在
+    try:
+        req = requests.get(url)
+    except requests.exceptions.MissingSchema:
+        print('Invalid URL "{}"'.format(url))
+        return
+    filename = url.split('/')[-1]
+    with open(filename, 'w') as fobj:
+        fobj.write(req.content.decode('utf-8'))
+    print('Download over.')
+
+if __name__ == '__main__':
+    url = input('Enter a URL: ')
+    download(url)
